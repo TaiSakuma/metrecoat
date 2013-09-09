73b62ca9693604baa0317b7a93f6a62330b505c8
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-09 09:50:42 -0500

    copy module definitions for MET corrections from cff and cfi files

--- corr_cfg_04_corrTerms_actual.py
+++ corr_cfg_04_corrTerms_actual.py
@@ -43,8 +43,65 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxEv
 
 ##____________________________________________________________________________||
 process.load("JetMETCorrections.Type1MET.pfMETCorrections_cff")
+process.pfJetMETcorr = cms.EDProducer(
+    "PFJetMETcorrInputProducer",
+    src = cms.InputTag('ak5PFJets'),
+    offsetCorrLabel = cms.string("ak5PFL1Fastjet"),
+    jetCorrLabel = cms.string("ak5PFL1FastL2L3"), # NOTE: use "ak5PFL1FastL2L3" for MC / "ak5PFL1FastL2L3Residual" for Data
+    jetCorrEtaMax = cms.double(9.9),
+    type1JetPtThreshold = cms.double(10.0),
+    skipEM = cms.bool(True),
+    skipEMfractionThreshold = cms.double(0.90),
+    skipMuons = cms.bool(True),
+    skipMuonSelection = cms.string("isGlobalMuon | isStandAloneMuon")
+)
+
+process.pfCandMETcorr = cms.EDProducer(
+    "PFCandMETcorrInputProducer",
+    src = cms.InputTag('pfCandsNotInJet')
+)
+
+process.pfchsMETcorr = cms.EDProducer(
+    "PFchsMETcorrInputProducer",
+    src = cms.InputTag('offlinePrimaryVertices'),
+    goodVtxNdof = cms.uint32(4),
+    goodVtxZ = cms.double(24)
+)
+
+##____________________________________________________________________________||
 process.load("JetMETCorrections.Type1MET.pfMETCorrectionType0_cfi")
-process.load("JetMETCorrections.Type1MET.caloMETCorrections_cff")
+process.pfMETcorrType0 = cms.EDProducer(
+    "Type0PFMETcorrInputProducer",
+    srcPFCandidateToVertexAssociations = cms.InputTag('pfCandidateToVertexAssociation'),
+    srcHardScatterVertex = cms.InputTag('selectedPrimaryVertexHighestPtTrackSumForPFMEtCorrType0'),
+    correction = cms.PSet(
+        formula = cms.string("-([0] + [1]*x)*(1.0 + TMath::Erf(-[2]*TMath::Power(x, [3])))"),
+        par0 = cms.double(0.),
+        par1 = cms.double(-0.703151),
+        par2 = cms.double(0.0303531),
+        par3 = cms.double(0.909209)
+    ),
+    minDz = cms.double(0.2) # [cm], minimum distance required between pile-up vertices and "hard scatter" vertex
+)
+
+##____________________________________________________________________________||
+# process.load("JetMETCorrections.Type1MET.caloMETCorrections_cff")
+process.caloJetMETcorr = cms.EDProducer(
+    "CaloJetMETcorrInputProducer",
+    src = cms.InputTag('ak5CaloJets'),
+    jetCorrLabel = cms.string("ak5CaloL2L3"), # NOTE: use "ak5CaloL2L3" for MC / "ak5CaloL2L3Residual" for Data
+    jetCorrEtaMax = cms.double(9.9),
+    type1JetPtThreshold = cms.double(20.0),
+    skipEM = cms.bool(True),
+    skipEMfractionThreshold = cms.double(0.90),
+    srcMET = cms.InputTag('corMetGlobalMuons')
+)
+
+process.muonCaloMETcorr = cms.EDProducer(
+    "MuonMETcorrInputProducer",
+    src = cms.InputTag('muons'),
+    srcMuonCorrections = cms.InputTag('muonMETValueMapProducer', 'muCorrData')
+)
 
 ##____________________________________________________________________________||
 process.p = cms.Path(
@@ -57,7 +114,8 @@ process.p = cms.Path(
     process.pfchsMETcorr +
     process.caloJetMETcorr +
     process.muonCaloMETcorr +
-    process.type0PFMEtCorrection
+    process.type0PFMEtCorrectionPFCandToVertexAssociation +
+    process.pfMETcorrType0
 )
 
 process.e1 = cms.EndPath(
