943f1b0d8e0202d2f6784d4b2eb425e769dd3211
Tai Sakuma <tai.sakuma@gmail.com>
2013-10-06 14:29:16 -0500

    rename reco_print_valuemap.py reco_print_MuonMETCorrectionData.py and update

--- reco_print_MuonMETCorrectionData.py
+++ reco_print_MuonMETCorrectionData.py
@@ -20,8 +20,13 @@ def main():
     
     print '%4s'  % 'run',
     print '%5s'  % 'event',
-    print '%25s' % 'module',
-    print '%7s'  % 'process',
+    print '%27s' % 'module',
+    print '%12s' % 'label',
+    print '%4s'  % 'type',
+    print '%10s' % 'x',
+    print '%10s' % 'y',
+    print '%10s' % 'pt',
+    print
     if getNEvents(inputPath):
         count(inputPath)
 
@@ -55,21 +60,53 @@ def count(inputPath):
         eventId = event.eventAuxiliary().event()
 
         event.getByLabel("muons", handleMuons)
-        print handleMuons.product()
-        print [m for m in handleMuons.product()]
-        print ROOT.reco.MuonRef
-        # for i in range( handleMuons.product().size()):
-        #    print ROOT.reco.MuonRef(handleMuons, i)
+
+        # ROOT.reco.MuonRef
+        # It is not clear how to initialize ROOT.reco.MuonRef with reco::Muon.
+        # in C++ (e.g. RecoMET/METAlgorithms/src/TCMETAlgo.cc),
+        #|| event.getByToken(muonToken_ , muonHandle_);
+        #|| event.getByToken(muonDepValueMapToken_, muonDepValueMapHandle_);
+        #|| for(unsigned int i = 0; i < muonHandle_->size(); ++i)
+        #||   {
+        #||     reco::MuonRef muonRef(muonHandle_, i);
+        #||     reco::MuonMETCorrectionData muCorrData = (*muonDepValueMapHandle_)[muonRef]
+        #||   }
+        #
+        # The equivalent code in FWLite.Python doesn't work
+        # First, Handle doesn't have size().
+        # use Handle.product().size instead
+        #|| for i in range(handleMuons.product().size()):
+        #||    muonref = ROOT.reco.MuonRef(handleMuons, i) # <- error, cannot instantiate in this way
+        #>>> TypeError: none of the 12 overloaded methods succeeded. Full details:
+        #
+        # However, ROOT.reco.MuonRef can be instantiated with no argument.
+        #|| print ROOT.reco.MuonRef()
+        #>>> <ROOT.edm::Ref<vector<reco::Muon>,reco::Muon,edm::refhelper::FindUsingAdvance<vector<reco::Muon>,reco::Muon> > object at 0x11757afa0>
+        #
+        # Second, ValueMap has no attribute '__getitem__' (probably equivalent
+        # of operator[]) so even if ROOT.reco.MuonRef can be initialized. cannot
+        # retrieve reco::MuonMETCorrectionData from the ValueMap
+        #|| event.getByLabel(("muonMETValueMapProducer",   "muCorrData", "METP"), handleValueMap)
+        #|| valuemap = handleValueMap.product()
+        #|| print valuemap[ROOT.reco.MuonRef()] # <-- error
+        #>>> TypeError: 'edm::ValueMap<reco::MuonMETCorrectionData>' object has no attribute '__getitem__'
 
         for ValueMap in ValueMaps:
             handle = ValueMap[3]
             event.getByLabel(ValueMap[0:3], handle)
             valuemap = handle.product()
-            # print valuemap.size()
-            # print valuemap.ids()
-            # print [valuemap.get(id) for id in valuemap.ids()]
-            print dir(valuemap)
-            print valuemap.get(0)
+
+            for i in range(valuemap.size()):
+                muonMETCorrectionData = valuemap.get(i)
+                print '%4d'    % run,
+                print '%5d'    % eventId,
+                print '%27s'   % ValueMap[0],
+                print '%12s'   % ValueMap[1],
+                print '%4d'    % muonMETCorrectionData.type(),
+                print '%10.3f' % muonMETCorrectionData.x(),
+                print '%10.3f' % muonMETCorrectionData.y(),
+                print '%10.3f' % muonMETCorrectionData.pt(),
+                print
 
 
 ##____________________________________________________________________________||
